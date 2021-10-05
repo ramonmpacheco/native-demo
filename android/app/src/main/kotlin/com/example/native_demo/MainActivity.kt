331ee056524245f0347b16bde568a9dcfcffee86
Ramon Pacheco <rmpacheco2@gmail.com>
2021-10-05 18:47:04 -0300

    add: native android code

--- android/app/src/main/kotlin/com/example/native_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_demo/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.example.native_demo
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "ramon.br/native"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if(call.method == "calcSum") {
+                val a = call.argument<Int>("a")?.toInt() ?: 0
+                val b = call.argument<Int>("b")?.toInt() ?: 0
+                result.success(a + b)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
